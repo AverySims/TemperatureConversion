9b985117bdcc9724e5740f07e558f6e8458d1717
AverySims <avery.j.sims@gmail.com>
2023-08-18 14:20:15 -0500

    Updating commit

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -2,9 +2,28 @@
 {
 	internal class Program
 	{
+		// array that lists all selectable conversion methods
+		private static const string[6] conversionTypes = ["Celsius to Fahrenheit",
+															"Fahrenheit to Celsius",
+															"Celsius to Kelvin",
+															"Kelvin to Celsius",
+															"Fahrenheit to Kelvin",
+															"Kelvin to Fahrenheit"];
+		private static void SelectConversionMethod()
+		{
+			Console.WriteLine("Select your conversion,");
+
+			for (int i = 0; i < conversionTypes.Length; i++)
+			{
+				Console.WriteLine(conversionTypes[i]);
+
+			}
+		}
+		
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			SelectConversionMethod();
+
 		}
 	}
 }
